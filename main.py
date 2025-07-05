46da08907f8b7f75418a1b86620e3ab8b98aece9
COMTIP <comtip1125@gmail.com>
2025-07-05 08:53:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import zeep
+
+app = FastAPI()
+
+# URL WSDL de Factory HKA
+wsdl = 'https://demoemision.thefactoryhka.com.pa/ws/obj/v1.0/Service.svc?singleWsdl'
+
+@app.post("/enviar-factura")
+async def enviar_factura(request: Request):
+    datos = await request.json()
+    datos['tokenEmpresa'] = "hqavyygdygrn_tfhka"
+    datos['tokenPassword'] = "@&Si-&7m/,dy"
+    try:
+        cliente = zeep.Client(wsdl=wsdl)
+        res = cliente.service.Enviar(**datos)
+        return JSONResponse(content={"respuesta": str(res)})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
